d734a672b5a8761abc21041ddef4692890c443e9
admin <bzqwangyi2017@163.com>
2020-12-07 14:50:05 +0800

    init common

---
+++ common/eureka_service1/src/main/java/com/springcloud/eureka_server1/EurekaServer1Application.java
@@ -0,0 +1,13 @@
+package com.springcloud.eureka_server1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer1Application.class, args);
+    }
+
+}
---
+++ common/eureka_service1/src/test/java/com/springcloud/eureka_server1/EurekaService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.eureka_server1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaService1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
